bc2c5a743840f5cac8ddffec68709540f3fc88ed
sakib911 <sakibhasan911@gmail.com>
2023-12-16 22:15:25 +0600

    Almost Done

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import "./Shop.css";
 import Product from "../Product/Product";
 
 const Shop = () => {
+    let selectedItem = 0;
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -11,17 +12,40 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  
+  const [cart, setCart] = useState([]);
+
+
+
+  // const totalItem=(props)=>{
+
+  //    document.getElementById('items').innerHTML = "Selected Item: "+ props
+  // }
+
+  const handleAddToCart = (product) => {
+
+    const newCart = [...cart, product];
+    setCart(newCart);
+    // selectedItem = selectedItem + 1;
+    // totalItem(selectedItem);
+  };
+
+ 
   return (
     <div className="shop-container">
       <div className="products-container">
 
         {console.log(products)}
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product key={product.id} 
+          product={product}
+          handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
-        <h2>Order here</h2>
+        <h2>Order Summary</h2>
+        <p >Selected Items: {cart.length}</p>
       </div>
     </div>
   );
