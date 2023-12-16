6521e09d4c7d98c44c59a7ca324b972b85ffbd47
sakib911 <sakibhasan911@gmail.com>
2023-12-16 16:52:40 +0600

    shop card added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,21 @@
-
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    const {name, id , seller,img, price,quantity, stock,ratings} = props.product;
-    return (
-        <div className = 'product'>
-            <img src={img} alt="" />
-        </div>
-    );
+  const { name, id, seller, img, price, quantity, stock, ratings } =
+    props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} stars</p>
+      </div>
+      <button className= 'btn-cart'>Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
